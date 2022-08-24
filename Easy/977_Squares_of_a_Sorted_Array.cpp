a5ba0fb379710acce04d0020cd4ce844487de94c
sadender <104177003+sadender@users.noreply.github.com>
2022-08-24 11:41:31 +0800

    Algorithm

---
+++ Easy/977_Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res(nums.size());
+        int l = 0; int r = nums.size() - 1;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (abs(nums[l]) > nums[r]) {
+                res[i] = nums[l] * nums[l];
+                ++l;
+            } else {
+                res[i] = nums[r] * nums[r];
+                --r;
+            }
+        }
+        
+        return res;
+    }
+};
